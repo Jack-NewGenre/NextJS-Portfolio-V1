c5399f1cc3cb38f7268cfa11d9304888e8e44b05
Jack-NewGenre <jack.cox@newgenre.studio>
2025-12-04 12:04:33 +0000

    fix: update url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { PageObjectResponse, QueryDatabaseResponse } from "@notionhq/client/buil
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = process.env.VERCEL_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
     const posts: QueryDatabaseResponse = await fetchPages();
 
